5b320650c25664a5b7f8ab3895177aae1ae536d2
Jesse Chilcott <jesse.chilcott@smartsparrow.com>
2019-03-23 19:11:47 +1100

    sped up nvm intialization by replaceing $HOME with ~ This small change removed the better part of a second from startup see: https://github.com/creationix/nvm/issues/1277#issuecomment-455175289

--- configs.sh
+++ configs.sh
@@ -21,7 +21,7 @@ eval "$(rbenv init -)"
 #
 # NVM
 #
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="~/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
