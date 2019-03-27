5175b4ea511f72161b716d0433189c1d6affc368
Jesse Chilcott <jesse.chilcott@gmail.com>
2019-03-27 11:29:15 +1100

    improved linux configs

--- dotfile_settings.zsh
+++ dotfile_settings.zsh
@@ -1,6 +1,9 @@
-
+#!/bin/zsh
 export DOTFILES_PATH=${0:a:h}
 export DOTFILES_VENDOR_PATH="${DOTFILES_PATH}/vendor"
 export DOTFILES_BIN_PATH="${DOTFILES_PATH}/bin"
 export DOTFILES_CONFIG_PATH="${DOTFILES_PATH}/config"
+
+[[ "$(uname)" == "Darwin" ]] && IS_MAC_OS=1 || IS_MAC_OS=0
+[[ "$(uname)" == "Linux" ]] && IS_LINUX=1 || IS_LINUX=0
 
--- setup.zsh
+++ setup.zsh
@@ -4,15 +4,22 @@ source ${0:a:h}/dotfile_settings.zsh
 mkdir -p ${DOTFILES_VENDOR_PATH}
 mkdir -p ${DOTFILES_BIN_PATH}
 
+
+if [[ $IS_MAC_OS ]]; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew install rbenv bat neovim oni
+elif [[ $IS_LINUX ]]; then
+  sudo pacman -S yaourt 
+  yaourt -S oni bat neovim rbenv
+fi
+
+rbenv init
+
 git clone https://github.com/zplug/zplug ${DOTFILES_VENDOR_PATH}/zplug
 
 # this is might write to .zshrc... check, and if it does, remove it.
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 
-if [[ "$(uname)" = "Darwin" ]]; then
-  brew install rbenv bat neovim oni
-  rbenv init
-fi
 
 #setup bat to use the Nord theme
 mkdir -p ${DOTFILES_VENDOR_PATH}/bat_themes/themes
@@ -23,5 +30,6 @@ bat cache --source ${DOTFILES_VENDOR_PATH}/bat_themes --init
 ln -s ${DOTFILES_CONFIG_PATH}/init.vim ${HOME}/.config/nvim/init.vim
 
 # setup oni
+mv ${HOME}/.config/oni ${HOME}/.config/oni_bak
 ln -s ${DOTFILES_CONFIG_PATH}/oni ${HOME}/.config/oni
 
