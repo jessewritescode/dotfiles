55d773d92678e1ba6897cebeb32fd7f88d9b8d11
Jesse Chilcott <jesse.chilcott@gmail.com>
2019-03-27 15:54:36 +1100

    Added kitty and i3 configs

--- setup.zsh
+++ setup.zsh
@@ -1,4 +1,14 @@
 #!/bin/zsh
+
+# Notes for future Jesse:
+# 
+# if this is a fresh machine, you will want to set zsh as your default shell first
+# on arch: pacman -S zsh, followed by chsh -s /bin/zsh
+#
+# For i3wm scaling issues, change Xft.dpi to 220 and URxvt font xft:whatever:size=12
+
+
+
 source ${0:a:h}/dotfile_settings.zsh
 
 mkdir -p ${DOTFILES_VENDOR_PATH}
@@ -9,8 +19,13 @@ if [[ $IS_MAC_OS ]]; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   brew install rbenv bat neovim oni
 elif [[ $IS_LINUX ]]; then
+  # set i3wm config
+  mkdir -p ${HOME}/i3
+  ln -s ${DOTFILES_CONFIG_PATH}/i3.conf ${HOME}/.i3/config
+
+  # get packages
   sudo pacman -S yaourt 
-  yaourt -S oni bat neovim rbenv
+  yaourt -S oni bat neovim rbenv fira-code kitty
 fi
 
 rbenv init
@@ -27,6 +42,7 @@ git clone git@github.com:arcticicestudio/nord-sublime-text.git ${DOTFILES_VENDOR
 bat cache --source ${DOTFILES_VENDOR_PATH}/bat_themes --init
 
 # setup neovim
+mkdir -p ${HOME}/.config/nvim
 ln -s ${DOTFILES_CONFIG_PATH}/init.vim ${HOME}/.config/nvim/init.vim
 
 # setup oni
