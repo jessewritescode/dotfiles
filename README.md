f07c732c7695f391096e98a8c4727375140dc91c
Jesse Chilcott <jesse.chilcott@gmail.com>
2019-04-10 19:35:49 +1000

    delete it all

--- README.md
+++
@@ -1,8 +0,0 @@
-# My dotfiles
-
-These are a collection of scripts I find my self reinstalling every time I use a new machine.
-
-All scripts are loaded via `dotfiles.sh` and can be switched on or off in that file.  Additionally, `dotfiles.sh` also contains the key bindings used by the scripts.
-
-Use at your own risk.
-
--- config/filenav.zsh
+++
@@ -1,9 +0,0 @@
-
-# configure FZF to use fd (as to respect gitignore), to follow symoblic links
-export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
-
-# fuzzy search code
-findcode() {
-  ag --nobreak --nonumbers --noheading . | fzf --delimiter=: --nth=2 --preview 'bat --color=always --style=plain --theme Nord {1}' | cut -d: -f1
-}
-
