880da4b24a5d2bd44be2eaa8462f675a1ad34458
Jesse Chilcott <jesse.chilcott@gmail.com>
2019-04-11 22:25:35 +1000

    added git author settings

--- .setup.zsh
+++ .setup.zsh
@@ -10,3 +10,8 @@ curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/instal
 
 # source zshrc to continue installing zsh plugins
 source ~/.zshrc
+
+# setup git (you may want to change this)
+git config --global user.email "jesse.chilcott@gmail.com"
+git config --global user.name "Jesse Chilcott"
+
