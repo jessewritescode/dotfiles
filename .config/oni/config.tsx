0338a7184e3f678be1ccc990d46fe54918cd2df4
Jesse Chilcott <jesse.chilcott@smartsparrow.com>
2019-04-12 15:34:13 +1000

    defer most of oni to nvim's config

--- .config/oni/config.tsx
+++ .config/oni/config.tsx
@@ -23,17 +23,25 @@ export const deactivate = (oni: Oni.Plugin.Api) => {
 }
 
 export const configuration = {
-  //add custom config here, such as
-  "oni.hideMenu": true
-  "ui.colorscheme": "nord",
-  "ui.animations.enabled": true,
-  "ui.fontSmoothing": "auto",
-  "experimental.markdownPreview.enabled": true,
-  "editor.quickOpen.execCommand": 'fzf -f ${searchString}',
-  "tabs.mode": "native",
-  "oni.useDefaultConfig": false,
-  "editor.fontFamily": "Fira Code Retina",
-  "experimental.vcs.sidebar": true,
-  "autoClosingPairs.enabled": false,
-  "statusbar.enabled": false
+    activate,
+    "ui.colorscheme": "nord",
+    "editor.quickOpen.execCommand": 'fzf -f ${searchString}',
+    "experimental.markdownPreview.enabled": true,
+    "oni.hideMenu"             : "hidden", // Hide top bar menu
+    "editor.fontFamily": "Fira Code",
+    "ui.fontSmoothing": "auto",
+    "ui.animations.enabled": true,
+    "oni.loadInitVim"          : true, // Load user's init.vim
+    "oni.useDefaultConfig"     : false, // Do not load Oni's init.vim
+    "autoClosingPairs.enabled" : false, // disable autoclosing pairs
+    "commandline.mode"         : false, // Do not override commandline UI
+    "wildmenu.mode"            : false, // Do not override wildmenu UI,
+    "tabs.mode"                : "native", // Use vim's tabline, need completely quit Oni and restart a few times
+    "statusbar.enabled"        : false, // use vim's default statusline
+    "sidebar.enabled"          : true, // sidebar ui is gone
+    "sidebar.default.open"     : true, // the side bar collapse 
+    "learning.enabled"         : false, // Turn off learning pane
+    "achievements.enabled"     : false, // Turn off achievements tracking / UX
+    "editor.typingPrediction"  : false, // Wait for vim's confirmed typed characters, avoid edge cases
+    "editor.textMateHighlighting.enabled" : false, // Use vim syntax highlighting
 }
