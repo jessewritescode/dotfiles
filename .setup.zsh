502ddfcdc5a937506bfa2182786593c43a5fcef3
Jesse Chilcott <jesse.chilcott@gmail.com>
2019-04-13 17:49:39 +1000

    set up npm repo to be local

--- .setup.zsh
+++ .setup.zsh
@@ -2,6 +2,12 @@
 sudo pacman -S yaourt 
 yaourt -S oni bat neovim rbenv fira-code kitty polybar the_silver_searcher rofi python-neovim zathura zathura-pdf-mupdf zathura-djvu zathura-ps zathura-cb perl-anyevent-i3 perl-json-xs jq 
 
+# setup npm and fetch packages
+mkdir -p ~/.npmlocal
+npm config set prefix ~/.npmlocal
+
+npm install -g nwb
+
 # install brew package manager
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
