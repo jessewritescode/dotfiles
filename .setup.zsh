c6149cde6139dcbd796726daff4b352cfce2463e
Jesse Chilcott <jesse.chilcott@smartsparrow.com>
2019-05-19 19:35:48 +1000

    cherrypicking macos

--- .setup.zsh
+++ .setup.zsh
@@ -8,8 +8,6 @@ npm install -g nwb
 # install brew package manager
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-sudo pip2 install --upgrade neovim
-
 # setup npm and fetch packages
 mkdir -p ~/.npmlocal
 npm config set prefix ~/.npmlocal
