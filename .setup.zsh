09881efe9d4e68afccdf7a9248700927aceb03f9
Jesse Chilcott <jesse.chilcott@gmail.com>
2019-06-14 11:22:04 +1000

    Fixing linux setup files

--- .setup.zsh
+++ .setup.zsh
@@ -8,15 +8,6 @@ npm config set prefix ~/.npmlocal
 
 npm install -g nwb
 
-# setup npm and fetch packages
-mkdir -p ~/.npmlocal
-npm config set prefix ~/.npmlocal
-
-npm install -g nwb
-
-# install brew package manager
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
 # install zplug for zshplugins
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
